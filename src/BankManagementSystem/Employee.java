ae08d472346467f7ac7eb04f3de6924abe9289ab
Zizokimo15 <163547727+Zizokimo15@users.noreply.github.com>
2024-12-16 00:28:44 +0200

    Update Employee.java

--- src/BankManagementSystem/Employee.java
+++ src/BankManagementSystem/Employee.java
@@ -1,74 +1,45 @@
 package BankManagementSystem;
-
-import java.util.Scanner;
-
 public class Employee {
-    public int id;
-    public static String firstName;
-    public static String lastName;
-    public static String address;
-    public static String position;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String position;
+    private String graduatedCollege;
+    private int yearOfGraduation;
+    private String totalGrade;
 
-    public Employee(int id,String firstName,String lastName,String address, String position) {
-        this.id=id;
-        Employee.firstName = firstName;
-        Employee.lastName = lastName;
-        Employee.address = address;
-        Employee.position = position;
+    private static int idCounter = 1;
+
+    public Employee(String firstName, String lastName, String address, String position, String graduatedCollege, int yearOfGraduation, String totalGrade) {
+        this.id = idCounter++;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.position = position;
+        this.graduatedCollege = graduatedCollege;
+        this.yearOfGraduation = yearOfGraduation;
+        this.totalGrade = totalGrade;
     }
 
+    public int getId() {
+        return id;
+    }
 
-
-    public static void editInfo(String address, String position) {
+    public void editInfo(String address, String position) {
         if (address != null && !address.isEmpty()) {
-            Employee.address = address;
+            this.address = address;
         }
         if (position != null && !position.isEmpty()) {
-            Employee.position = position;
+            this.position = position;
         }
         System.out.println("Employee information updated.");
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public static String getName() {
-        return firstName+lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
+    @Override
     public String toString() {
-        return "Employee{" +
-                ", name='" + firstName + " " + lastName + '\'' +
-                ", Address: " + address +
-                ", position='" + position + '\'' +
-                '}';
-    }
-
-    public static Employee createEmployee() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter employee ID: ");
-        int id = scanner.nextInt();
-        scanner.nextLine(); // consume newline
-        System.out.print("Enter employee first name: ");
-        String firstName = scanner.nextLine();
-        System.out.print("Enter employee last name: ");
-        String lastName = scanner.nextLine();
-        System.out.print("Enter employee position: ");
-        String position = scanner.nextLine();
-        System.out.print("Enter employee address: ");
-        String address = scanner.nextLine();
-        return new Employee(id, firstName, lastName,address,position);
+        return "Employee[ID=" + id + ", Name=" + firstName + " " + lastName + ", Address=" + address +
+                ", Position=" + position + ", College=" + graduatedCollege + ", Year=" + yearOfGraduation +
+                ", Grade=" + totalGrade + "]";
     }
 }
-
